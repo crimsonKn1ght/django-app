8685fe5ccf3a29f857e78f97d03e9f92988cb7ed
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2025-12-31 19:21:42 +0530

    added forms to add tasks

--- app/home/models.py
+++ app/home/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class taskLists(models.Model):
     task_name = models.CharField(max_length=100, null=False, blank=False)
     task_description = models.TextField(max_length=100)
-    task_time = models.TimeField(null=True, blank=True)
+    task_time = models.TextField(null=True, blank=True)
+    task_status = models.TextField(null=True, blank=True)
--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
@@ -9,10 +10,42 @@ def home(request):
                                                         'alt_desc': 'time bunny meme'})
 
 def tasks(request):
-    task_chart = [
-        {'task_name': 'Exercise', 'task_description': '5 sets of sit-ups\n5sets of pull-ups', 'task_time': '8AM', 'task_status': 'Done'},
-        {'task_name': 'Breakfast', 'task_description': 'Have ample food for a good morning', 'task_time': '8:30AM', 'task_status': 'Not-Done'},
-        {'task_name': 'Read books', 'task_description': 'Read 10 pages of any book', 'task_time': '9AM', 'task_status': 'Done'},
-        {'task_name': 'Online meeting', 'task_description': 'Progress report on project', 'task_time': '10AM', 'task_status': 'Done'},
-    ]
+    task_chart = []
+    # task_chart = [
+    #     {'task_name': 'Exercise', 'task_description': '5 sets of sit-ups\n5sets of pull-ups', 'task_time': '8AM', 'task_status': 'Done'},
+    #     {'task_name': 'Breakfast', 'task_description': 'Have ample food for a good morning', 'task_time': '8:30AM', 'task_status': 'Not-Done'},
+    #     {'task_name': 'Read books', 'task_description': 'Read 10 pages of any book', 'task_time': '9AM', 'task_status': 'Done'},
+    #     {'task_name': 'Online meeting', 'task_description': 'Progress report on project', 'task_time': '10AM', 'task_status': 'Done'},
+    # ]
+
+    queryset = taskLists.objects.all()
+    lenq = len(queryset)
+
+    for i in range(lenq):
+        task_chart.append({'task_name': queryset[i].task_name, \
+                        'task_description': queryset[i].task_description, \
+                            'task_time': queryset[i].task_time, \
+                                'task_status': queryset[i].task_status})
+
+    if request.method=="POST":
+        data = request.POST
+        task_name=data.get('task_name')
+        task_description=data.get('task_description')
+        task_time=data.get('task_time')
+        task_status=data.get('task_status')
+        print(task_name, task_description, task_time, task_status)
+
+        taskLists.objects.create(
+            task_name=task_name,
+            task_description=task_description,
+            task_time=task_time,
+            task_status=task_status,
+        )
+
+        # queryset = taskLists.objects.all()
+        task_chart.append({'task_name': task_name, \
+                        'task_description': task_description, \
+                            'task_time': task_time, \
+                                'task_status': task_status})
+    
     return render(request, 'home/tasks.html', context={'name': 'Tasks', 'task_chart': task_chart})
